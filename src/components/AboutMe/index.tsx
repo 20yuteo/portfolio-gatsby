7a558fb2c8c3f81ec3b04a67dce9ae21ef75532b
20yuteo <20yuteo@gmail.com>
2023-10-29 02:55:26 +0900

    About Meの見た目はできた

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -3,10 +3,65 @@ import SC from "./style";
 
 export const AboutMe = () => {
     return(
-        <SC.AboutMeSection>
-            <SC.AboutMeTitle>
-                About Me
-            </SC.AboutMeTitle>
-        </SC.AboutMeSection>
+        <SC.Section>
+            <SC.Body>
+                <SC.Title>
+                    About Me
+                </SC.Title>
+                <SC.ImgContainer>
+                    <SC.Img src={'/eto_ushi_head.png'} />
+                </SC.ImgContainer>
+                <SC.Content>
+                    <SC.ContentItem>
+                        <SC.ItemTitle>
+                            Name
+                        </SC.ItemTitle>
+                        <SC.ItemList>
+                            <SC.ListItem>
+                                Eto Ushi
+                            </SC.ListItem>
+                            <SC.ListItem>
+                                AAAAAA
+                            </SC.ListItem>
+                            <SC.ListItem>
+                                BBBB
+                            </SC.ListItem>
+                        </SC.ItemList>
+                    </SC.ContentItem>
+                    <SC.ContentItem>
+                        <SC.ItemTitle>
+                            Name
+                        </SC.ItemTitle>
+                        <SC.ItemList>
+                            <SC.ListItem>
+                                Eto Ushi
+                            </SC.ListItem>
+                            <SC.ListItem>
+                                AAAAAA
+                            </SC.ListItem>
+                            <SC.ListItem>
+                                BBBB
+                            </SC.ListItem>
+                        </SC.ItemList>
+                    </SC.ContentItem>
+                    <SC.ContentItem>
+                        <SC.ItemTitle>
+                            Name
+                        </SC.ItemTitle>
+                        <SC.ItemList>
+                            <SC.ListItem>
+                                Eto Ushi
+                            </SC.ListItem>
+                            <SC.ListItem>
+                                AAAAAA
+                            </SC.ListItem>
+                            <SC.ListItem>
+                                BBBB
+                            </SC.ListItem>
+                        </SC.ItemList>
+                    </SC.ContentItem>
+                </SC.Content>
+            </SC.Body>
+        </SC.Section>
     )
 }
--- src/components/AboutMe/style.ts
+++ src/components/AboutMe/style.ts
@@ -1,17 +1,86 @@
 import styled from 'styled-components';
 
-const AboutMeSection = styled.section`
+const Section = styled.section`
     padding: 80px;
     background-color: #FFF;
+    width: 100%;
 `;
 
-const AboutMeTitle = styled.h2`
+const Title = styled.h2`
     font-size: 48px;
-    font-weight: 600;
+    font-weight: 700;
     line-height: 1.2;
 `;
 
+const Body = styled.div`
+    max-width: 762px;
+    margin: 0 auto;
+`;
+
+const ImgContainer = styled.div`
+    width: 120px;
+    height: 120px;
+    flex-shrink: 0;
+    border-radius: 64px;
+    margin: 16px auto;
+    background-color: #DCFD00;
+    border: 4px solid var(--text-subtitle, #000);
+    overflow: hidden;
+`;
+
+const Img = styled.img`
+    width: 120px;
+    height: auto;
+`;
+
+const Content = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+`;
+
+const ContentItem = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    border-radius: 16px;
+    border: 4px solid #000;
+    background: #A4E8FF;
+    box-shadow: 0px -12px 1px 0px rgba(0, 0, 0, 0.25) inset;
+
+    width: 327px;
+    height: 180px;
+    padding: 12px;
+    gap: 10px;
+`;
+
+const ItemTitle = styled.h3`
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 1.2;
+    text-align: center;
+`;
+
+const ItemList = styled.ul`
+    list-style: none;
+    padding: 0;
+`;
+
+const ListItem = styled.li`
+    display: flex;
+    align-items: center;
+    padding: 8px 0;
+`;
+
 export default {
-    AboutMeSection,
-    AboutMeTitle,
+    Section,
+    Title,
+    Body,
+    ImgContainer,
+    Img,
+    Content,
+    ContentItem,
+    ItemTitle,
+    ItemList,
+    ListItem
 };
