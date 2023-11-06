f6c1463eb0ab3d96c97142e92e404e4fb3d66ac6
20yuteo <20yuteo@gmail.com>
2023-11-06 22:38:26 +0900

    Aboutページ修正

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,53 +1,36 @@
-import React from "react";
-import SC from "./style";
-import { ColorfulCard } from "../Card/ColorfulCard";
-import { Section } from "../Section";
+import React from 'react';
+import SC from './style';
+import { ColorfulCard } from '../Card/ColorfulCard';
+import { Section } from '../Section';
 
 export const AboutMe = () => {
-    return(
-        <Section>
-            <Section.Body>
-                <Section.Title>
-                    About Me
-                </Section.Title>
-                <SC.ImgContainer>
-                    <SC.Img src={'/eto_ushi_head.png'} />
-                </SC.ImgContainer>
-                <SC.Content>
-                    <ColorfulCard>
-                        <SC.ItemTitle>
-                            Jobs
-                        </SC.ItemTitle>
-                        <SC.ItemList>
-                            <SC.ListItem>
-                                Eto Ushi
-                            </SC.ListItem>
-                            <SC.ListItem>
-                                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-                            </SC.ListItem>
-                            <SC.ListItem>
-                                BBBB
-                            </SC.ListItem>
-                        </SC.ItemList>
-                    </ColorfulCard>
-                    <ColorfulCard>
-                        <SC.ItemTitle>
-                            Skills
-                        </SC.ItemTitle>
-                        <SC.ItemList>
-                            <SC.ListItem>
-                                Eto Ushi
-                            </SC.ListItem>
-                            <SC.ListItem>
-                                AAAAAA
-                            </SC.ListItem>
-                            <SC.ListItem>
-                                BBBB
-                            </SC.ListItem>
-                        </SC.ItemList>
-                    </ColorfulCard>
-                </SC.Content>
-            </Section.Body>
-        </Section>
-    )
-}
+  return (
+    <Section>
+      <Section.Body>
+        <Section.Title>About Me</Section.Title>
+        <SC.ImgContainer>
+          <SC.Img src={'/eto_ushi_head.png'} />
+        </SC.ImgContainer>
+        <SC.Content>
+          <ColorfulCard>
+            <SC.ItemTitle>Introduce Myself</SC.ItemTitle>
+            <SC.ItemList>
+              <SC.ListItem>✏️: 20yuteo</SC.ListItem>
+              <SC.ListItem>👨‍💻: Web Engneer</SC.ListItem>
+            </SC.ItemList>
+          </ColorfulCard>
+          <ColorfulCard>
+            <SC.ItemTitle>Skills</SC.ItemTitle>
+            <SC.ItemList>
+              <SC.ListItem>FrontEnd: React.js</SC.ListItem>
+              <SC.ListItem>FrontEnd: Vue.js</SC.ListItem>
+              <SC.ListItem>BackEnd: Laravel</SC.ListItem>
+              <SC.ListItem>BackEnd: Java</SC.ListItem>
+              <SC.ListItem>BackEnd: Ruby on Rails</SC.ListItem>
+            </SC.ItemList>
+          </ColorfulCard>
+        </SC.Content>
+      </Section.Body>
+    </Section>
+  );
+};
